a4218f10b29556fae34b00143f874049f1662f16
KMorcinek <KMorcinek@b0cb2f60-a98f-4b4f-b612-212f15a34d4a>
2014-03-03 12:15:08 +0000

    ViewModelBase
    git-svn-id: svn://making-svn/Making%20Waves/MakingWaves.Tools.AppChooser/trunk@147451 b0cb2f60-a98f-4b4f-b612-212f15a34d4a

--- src/ApplicationChooser/AppItemViewModel.cs
+++ src/ApplicationChooser/AppItemViewModel.cs
@@ -1,82 +1,72 @@
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Windows;
 
 namespace ApplicationChooser
 {
-    public class AppItemViewModel : INotifyPropertyChanged
+    public class AppItemViewModel : ViewModelBase
     {
-        private readonly AppItem appItem;
+        private readonly AppItem _appItem;
         public AppItem AppItem
         {
-            get { return appItem;}
+            get { return _appItem;}
         }
 
-        private bool isSelected;
+        private bool _isSelected;
         public bool IsSelected
         {
             get
             {
-                return !IsOptional || isSelected || SubApps.Exists(s => s.IsSelected);
+                return !IsOptional || _isSelected || SubApps.Exists(s => s.IsSelected);
             }
             set
             {
-                isSelected = value;
+                _isSelected = value;
                 SubApps.ForEach(c => c.IsSelected = value);
 
                 //select parent when child selected
                 if (Parent != null && value)
                     Parent.Select();
 
-                OnPropertyChanged("IsSelected");
+                OnPropertyChanged(() => IsSelected);
             }
         }
 
         public bool IsOptional
         {
-            get { return !appItem.IsRequired && !SubApps.Exists(s => s.appItem.IsRequired); }
+            get { return !_appItem.IsRequired && !SubApps.Exists(s => s._appItem.IsRequired); }
         }
 
         public string Name
         {
-            get { return appItem.Name; }
+            get { return _appItem.Name; }
         }
 
-        private AppItemViewModel parent;
+        private AppItemViewModel _parent;
         public AppItemViewModel Parent
         {
-            get { return parent; }
+            get { return _parent; }
             set
             {
-                parent = value;
-                OnPropertyChanged("Parent");
+                _parent = value;
+                OnPropertyChanged(() => Parent);
             }
         }
 
-        private readonly List<AppItemViewModel> subApps = new List<AppItemViewModel>();
+        private readonly List<AppItemViewModel> _subApps = new List<AppItemViewModel>();
 
         public List<AppItemViewModel> SubApps
         {
-            get { return subApps; }
+            get { return _subApps; }
         }
-
-        public event PropertyChangedEventHandler PropertyChanged;
 
         public AppItemViewModel(AppItem item)
         {
-            appItem = item;
-        }
-
-        protected void OnPropertyChanged(string propertyName)
-        {
-            if (PropertyChanged != null)
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            _appItem = item;
         }
 
         private void Select()
         {
-            isSelected = true;
-            OnPropertyChanged("IsSelected");
+            _isSelected = true;
+            OnPropertyChanged(() => IsSelected);
         }
     }
 }
